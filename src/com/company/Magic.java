c50a6b9e213e088a0902988f35e8cc957c72ebe1
user2 <ddjem9931@gmail.com>
2022-04-10 19:41:06 +0300

    Initial commit

---
+++ src/com/company/Magic.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Magic extends Hero {
+    public void applySuperAbiliti(String superAbilityType) {
+        System.out.println("Magic применил способность летать ");
+    }
+}
---
+++ src/com/company/Warrior.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Warrior extends Hero {
+    public void applySuperAbiliti(String superAbilityType){
+        System.out.println("Warrior применил способность стихию воздуха");
+    }
+}
